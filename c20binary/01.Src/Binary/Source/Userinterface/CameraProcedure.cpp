577baa3385d0caced2894aa97a09759f8cb3aa26
SamuraiHUN <89939630+SamuraiHUN@users.noreply.github.com>
2021-09-30 21:35:38 +0200

    Update CameraProcedure.cpp

--- c20binary/01.Src/Binary/Source/Userinterface/CameraProcedure.cpp
+++ c20binary/01.Src/Binary/Source/Userinterface/CameraProcedure.cpp
@@ -28,3 +28,83 @@ change to:
 	}
 	else
 		SetCameraState(CAMERA_STATE_NORMAL);
+
+search this:
+
+		if (bCollide)
+		{
+			if (m_v3AngularVelocity.y > 0.0f)
+			{
+				m_v3AngularVelocity.y = 0.0f;
+				m_v3AngularVelocity.z += fMoveAmountLarge;
+			}
+	//		m_v3AngularVelocity.y = fMAX(fMoveAmountSmall, m_v3AngularVelocity.y);
+	//// 		m_v3AngularVelocity.y += fMoveAmountSmall;
+
+			if (kVct_kPosition.size() > 1)
+			{
+	//			m_v3AngularVelocity.z = fMAX(fMoveAmountSmall, m_v3AngularVelocity.z);
+ 				m_v3AngularVelocity.z += fMoveAmountSmall;
+			}
+			else
+			{
+				D3DXVec3Cross(&v3CheckVector, &(kVct_kPosition[0] - m_v3Eye), &m_v3View);
+				float fDot = D3DXVec3Dot(&v3CheckVector, &m_v3Up);
+				if (fDot < 0)
+				{
+	//				m_v3AngularVelocity.x = fMIN(-fMoveAmountSmall, m_v3AngularVelocity.x);
+ 					m_v3AngularVelocity.x -= fMoveAmountSmall;
+				}
+				else if(fDot > 0)
+				{
+	//				m_v3AngularVelocity.x = fMAX(fMoveAmountSmall, m_v3AngularVelocity.x);
+ 					m_v3AngularVelocity.x += fMoveAmountSmall;
+				}
+				else
+				{
+	//				m_v3AngularVelocity.z = fMAX(fMoveAmountSmall, m_v3AngularVelocity.z);
+ 					m_v3AngularVelocity.z += fMoveAmountSmall;
+				}
+			}
+		}
+
+
+change to:
+
+		if (bCollide)
+		{
+			if (m_v3AngularVelocity.y > 0.0f)
+			{
+				m_v3AngularVelocity.y = 0.0f;
+				m_v3AngularVelocity.z += fMoveAmountLarge;
+			}
+	//		m_v3AngularVelocity.y = fMAX(fMoveAmountSmall, m_v3AngularVelocity.y);
+	//// 		m_v3AngularVelocity.y += fMoveAmountSmall;
+
+			if (kVct_kPosition.size() > 1)
+			{
+	//			m_v3AngularVelocity.z = fMAX(fMoveAmountSmall, m_v3AngularVelocity.z);
+ 				m_v3AngularVelocity.z += fMoveAmountSmall;
+			}
+			else
+			{
+				auto d3dd = (kVct_kPosition[0] - m_v3Eye);
+				D3DXVec3Cross(&v3CheckVector, &d3dd, &m_v3View);
+				float fDot = D3DXVec3Dot(&v3CheckVector, &m_v3Up);
+				if (fDot < 0)
+				{
+	//				m_v3AngularVelocity.x = fMIN(-fMoveAmountSmall, m_v3AngularVelocity.x);
+ 					m_v3AngularVelocity.x -= fMoveAmountSmall;
+				}
+				else if(fDot > 0)
+				{
+	//				m_v3AngularVelocity.x = fMAX(fMoveAmountSmall, m_v3AngularVelocity.x);
+ 					m_v3AngularVelocity.x += fMoveAmountSmall;
+				}
+				else
+				{
+	//				m_v3AngularVelocity.z = fMAX(fMoveAmountSmall, m_v3AngularVelocity.z);
+ 					m_v3AngularVelocity.z += fMoveAmountSmall;
+				}
+			}
+		}
